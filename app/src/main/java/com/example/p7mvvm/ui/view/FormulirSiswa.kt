813081d1891c81ef30a72b51629a581e51685ebc
LingGrani <luthfiangger2510@gmail.com>
2024-11-21 19:56:09 +0700

    add view "FormulirSiswa"

---
+++ app/src/main/java/com/example/p7mvvm/ui/view/FormulirSiswa.kt
@@ -0,0 +1,111 @@
+package com.example.p7mvvm.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.p7mvvm.ui.theme.Purple80
+import com.example.p7mvvm.ui.theme.green12
+
+@Composable
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+) {
+    var nama by remember { mutableStateOf("")}
+    var email by remember { mutableStateOf("")}
+    var alamat by remember { mutableStateOf("")}
+    var nomortelepon by remember { mutableStateOf("")}
+    var gender by remember { mutableStateOf("")}
+
+    val listData : MutableList<String> = mutableListOf(nama, gender, email, alamat, nomortelepon)
+
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text("Nama") },
+            placeholder = { Text("isi nama anda")},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            listJK.forEach { SelectedGender ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = gender == SelectedGender,
+                        onClick = {gender = SelectedGender}
+                    )
+                    Text(
+                        text = SelectedGender
+                    )
+                }
+            }
+        }
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            placeholder = { Text("isi Email anda")},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        TextField(
+            value = alamat,
+            onValueChange = { alamat = it },
+            label = { Text("Alamat") },
+            placeholder = { Text("isi Alamat anda")},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        TextField(
+            value = nomortelepon,
+            onValueChange = { nomortelepon = it },
+            label = { Text("Nomor Telepon") },
+            placeholder = { Text("isi Nomor Telepon anda")},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Button(
+            onClick = {
+                onSubmitClicked(listData)
+            }
+        ) {
+            Text(
+                text = "Simpan",
+                )
+        }
+    }
+}

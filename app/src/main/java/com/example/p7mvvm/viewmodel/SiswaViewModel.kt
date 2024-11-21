1c0978aba8d62a116db3403a745d41eed9f10fc0
LingGrani <luthfiangger2510@gmail.com>
2024-11-21 19:57:29 +0700

    add ViewModel "SiswaViewModel"

---
+++ app/src/main/java/com/example/p7mvvm/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.p7mvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.p7mvvm.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(DataSiswa())
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun SaveDataSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                jenisKelamin = ls[1],
+                email = ls[2],
+            )
+        }
+    }
+}

68c361f86ad9a52d6f636d2fee84e1137684e2f7
LingGrani <luthfiangger2510@gmail.com>
2024-11-21 19:58:43 +0700

    Update MainActivity Navigation

--- app/src/main/java/com/example/p7mvvm/MainActivity.kt
+++ app/src/main/java/com/example/p7mvvm/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             P7MVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavigationControl(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    P7MVVMTheme {
-        Greeting("Android")
     }
 }
